32bd93963f8a56e636d76581d974c5a84cdeec06
Saleem Abbas <saleemabbasfrauas@gmail.com>
2024-03-21 06:09:48 +0100

    create factory method to simplify test setup

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -4,42 +4,19 @@
 //
 //  Created by abbas on 09.12.23.
 //
-
-/// goal is to get rid of the singeltons ,
-/// 1. make the shared instance a variable instead of let so it would not be anymore a singelton , now shared is a global state not a singelton
-/// 2. move the test logic from remotefeedloader to httpclient
-/// 3. Move the test logic to a new subclass of the HTTPClient , instead of directly touching the production code the goal is i want to access into my test target to test the code
-/// 4. Done! We dont have a Singelton anymore and the test logic is now in a test type (the spy).
-///
-///  Refactoring Points
-///  1. HTTPClient.shared.get is mixing the responsibilities here , looking for a method (get) in an object  and responsibility of locating this object using shared.
-///  If we inject our client it would be more testable
-///  So rather using Subclasing we can use composition , we can start injecting into the feedloader.
-///  Now you can use protocol to define the interfacces because HTTPClient is an abstract class now
-///  give a url from outside instead hardcoding inside the RemoteFeedLoader, we moved all the details to the test side and code is getting more generic
-/// to remove the duplication of code use the factory method to make System Under Test SUT
-///
-///  outcomes :
-/// 1.  singelton progression to eliminating the tight coupling by using dependency injection.
-/// 2. there is no reason to make the httpclient a singelton or a shared instance apart from conveience to locate the instance directly.
-///  3. for creation of singeltons you need to have a good reason for example you want to have only one HTTPClient per application.
-///  4. by introducing the clean separation with protocols, we made the remotefeedloader more flexible, open for extension and more testable
-///   5. refactoring by backing up by tests is a very power ful tool, without tests you will have a feat of change
-///
-/// when testing objects collaborating, asserting the values passed is not enough we also need to ask how many times as the method invoked?
-/// mistakes like this happen all the time especially when merging the code
-
-@testable import EssentialFeed
 import XCTest
+import EssentialFeed
 
-final class RemoteFeedLoaderTests: XCTestCase {
+class RemoteFeedLoaderTests: XCTestCase {
+    
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
+        
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
-    func test_load_requestDataFromURL() {
-        let url = URL(string: "https://a-given-url-com")!
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
         sut.load { _ in }
@@ -47,8 +24,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
-    func test_loadTwice_requestDataFromURL() {
-        let url = URL(string: "https://a-given-url-com")!
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
         sut.load { _ in }
@@ -60,65 +37,104 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(.connectivity)) {
+        expect(sut, toCompleteWith: .failure(.connectivity), when: {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
-        }
+        })
     }
     
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
+        
         let samples = [199, 201, 300, 400, 500]
         
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: .failure(.invalidData)) {
-                client.complete(withStatusCode: 200, at: index)
-            }
+            expect(sut, toCompleteWith: .failure(.invalidData), when: {
+                client.complete(withStatusCode: code, at: index)
+            })
         }
     }
     
-    func test_load_deliversErrorOn200HTTPResponseWithInvalidData() {
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(.invalidData)) {
-            let invalidJSON = Data(bytes: "Invalid Data", count: 200)
+        expect(sut, toCompleteWith: .failure(.invalidData), when: {
+            let invalidJSON = Data(bytes: "invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
-        }
+        })
     }
     
     func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
         let (sut, client) = makeSUT()
         
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
-        
-        expect(sut, toCompleteWith: .success([])) {
+        expect(sut, toCompleteWith: .success([]), when: {
             let emptyListJSON = Data(bytes: "{\"items\": []}".utf8)
             client.complete(withStatusCode: 200, data: emptyListJSON)
-        }
+        })
     }
     
-    // MARK: Helpers
+    func test_load_deliversItemsOn200HTTPResponseWithJSONItems() {
+        let (sut, client) = makeSUT()
+        
+        let item1 = makeItem(
+            id: UUID(),
+            description: nil,
+            location: nil,
+            imageURL: URL(string: "http://a-url.com")!)
+        
+        let item2 = makeItem(
+            id: UUID(),
+            description: "a description",
+            location: "a location",
+            imageURL: URL(string: "http://another-url.com")!)
+        
+        let items = [item1.model, item2.model]
+        
+        
+        expect(sut, toCompleteWith: .success(items), when: {
+            let json = makeItemsJSON([item1.json, item2.json])
+            client.complete(withStatusCode: 200, data: json)
+        })
+    }
     
-    private func makeSUT(url: URL = URL(string: "https://a-given-url-com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut, client)
     }
     
-    private func expect(_ sut: RemoteFeedLoader, toCompleteWith result: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
+        let item = FeedItem(id: id, description: description, location: location, imageURL: imageURL)
         
-        var capturedErrors =  [RemoteFeedLoader.Result]()
-        sut.load { capturedErrors.append($0) }
+        let json = [
+            "id": id.uuidString,
+            "description": description,
+            "location": location,
+            "image": imageURL.absoluteString
+        ].reduce(into: [String: Any]()) { (acc, e) in
+            if let value = e.value { acc[e.key] = value }
+        }
+        
+        return (item, json)
+    }
+    
+    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        let json = ["items": items]
+        return try! JSONSerialization.data(withJSONObject: json)
+    }
+    
+    private func expect(_ sut: RemoteFeedLoader, toCompleteWith result: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { capturedResults.append($0) }
         
         action()
         
-        XCTAssertEqual(capturedErrors, [result], file: file, line: line)
+        XCTAssertEqual(capturedResults, [result], file: file, line: line)
     }
     
-    
     private class HTTPClientSpy: HTTPClient {
-        
         private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
         
         var requestedURLs: [URL] {
@@ -134,8 +150,15 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
         
         func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
-            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )!
             messages[index].completion(.success(data, response))
         }
     }
+    
 }
+
